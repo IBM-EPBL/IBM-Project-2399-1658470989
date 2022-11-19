ea5073020f43498ee84f3f37450af52c58b62373
Samyubalu <113822232+Samyubalu@users.noreply.github.com>
2022-11-19 18:23:24 +0530

    Add files via upload

---
+++ Project Design & planning/Project Development Phase/SPRINT 4/FLASK INTEGRATION/Flask_Integration.py
@@ -0,0 +1,15 @@
+import requests
+
+# NOTE: you must manually set API_KEY below using information retrieved from your IBM Cloud account.
+API_KEY = "<your API key>"
+token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey":
+ API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
+mltoken = token_response.json()["access_token"]
+
+header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
+
+# NOTE: manually define and pass the array(s) of values to be scored in the next line
+payload_scoring = {"input_data": [{"fields": [array_of_input_fields], "values": [array_of_values_to_be_scored, another_array_of_values_to_be_scored]}]}
+
+response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/e52118de-5d9f-41b6-8348-69a063a245cf/predictions?version=2022-11-19', json=payload_scoring,
+ headers={'Authorization': 'Bearer ' + mltoken})
